70495dcba21b7dfffc267af3c4aa5bf3352f67aa
f(x) software <fofx.software@gmail.com>
2015-06-10 15:09:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,24 +12,41 @@ var headers = [
   'scheduled'
 ];
 
-var stopData = splitLines.map(function(line) {
+var routeRows = splitLines.map(function(line) {
   var obj = {}, line = line.split(',');
-  headers.forEach(function(header, index) {
-    obj[header] = line[index];
-  });
-  return obj;
+  if(line) {
+    headers.forEach(function(header, index) {
+      obj[header] = line[index];
+    });
+    return obj;
+  }
 });
 
-if(typeof stopData.slice(-1)[0].route === 'undefined') stopData.pop();
+if(!routeRows.slice(-1)[0]) routeRows.pop();
 
 function onlyUnique(value, index, self) { 
   return self.indexOf(value) === index;
 }
 
-var allStops = stopData.map(function(row, index) {
+var allStops = routeRows.map(function(row) {
   return row.stop;
 }).filter(onlyUnique);
 
-console.log(allStops);
+var stopData = {};
+
+allStops.forEach(function(stop) {
+  stopData[stop] = {
+    timeFromLast: [],
+    arrivalMinusScheduled: [],
+    departureMinusScheduled: []
+  };
+  var lastRow;
+  routeRows.forEach(function(row, index) {
+    var timeFromLast = row.arrival - lastRow.arrival;
+    var arrivalMinusScheduled = row.arrival - row.scheduled;
+    var departureMinusScheduled = row.departure - row.scheduled;
+    lastRow = row;
+  });
+});
 
 });
