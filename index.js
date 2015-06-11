4bfaedcde898fb144b4a991480b6b4b4a1c0bc1e
f(x) software <fofx.software@gmail.com>
2015-06-10 23:00:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -39,12 +39,22 @@ routeRows.some(function(row) {
   }
 });
 
-for(var i = 0; i < routeRows.length; i += allStops.length) {
-  for(var j = 0; j < routeRows.length; j += allStops.length) {
-    if(makeDate(routeRows[j].scheduled) > makeDate(routeRows[i].scheduled)) {
-      var removed = routeRows.splice(i, allStops.length);
-      routeRows.splice.apply(routeRows, [j, 0].concat(removed));
-      j = routeRows.length;
+for(var i = 0, found = false; i < routeRows.length && !found; i += allStops.length) {
+  if(routeRows[i].stop === allStops[0]) {
+    for(var j = 0; j < routeRows.length; j += allStops.length) {
+      if(makeDate(routeRows[j].scheduled) > makeDate(routeRows[i].scheduled)) {
+        var removed = routeRows.splice(i, allStops.length);
+        routeRows.splice.apply(routeRows, [j, 0].concat(removed));
+        found = true;
+      }
+    }
+  } else {
+    for(var k = 0, found2 = false; k < allStops.length && !found2; k++) {
+      i -= k;
+      if(routeRows[i].stop === allStops[0]) {
+        i -= allStops.length;
+        found2 = true;
+      }
     }
   }
 }
