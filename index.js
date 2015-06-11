4b4759e0efaf01a6bb116a61bf4e341d709a6d06
f(x) software <fofx.software@gmail.com>
2015-06-10 23:40:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ for(var i = 0; i < routeRows2.length; i += allStops.length) {
       routeRows = routeRows.concat(routeRows2.slice(i, i + allStops.length));
     }
   } else {
-    for(var k = i; routeRows2[k].stop !== allStops[0]; k--);
+    for(var k = i; k && routeRows2[k].stop !== allStops[0]; k--);
     i = k - allStops.length;
   }
 }
