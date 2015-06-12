70ef48dde71efa11851bb622afa3e3f8187cc48e
f(x) software <fofx.software@gmail.com>
2015-06-11 20:56:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -174,9 +174,9 @@ allStops.forEach(function(stop, stopIndex) {
     arriveDiffs[stopTime].forEach(function(diff) {
       var addTo = diff < 0 ? td1 : td2;
       var svg = addTo.find('svg')[0];
-      if(Math.abs(diff) > svg.getAttribute(width)) svg.setAttributeNS(svgNS, 'width', Math.abs(diff));
+      if(Math.abs(diff) > svg.getAttribute('width')) svg.setAttributeNS(svgNS, 'width', Math.abs(diff));
       var circle = document.createElementNS(svgNS, 'circle');
-      circle.setAttributeNS(svgNS, 'cx', svg.getAttribute(width) + diff);
+      circle.setAttributeNS(svgNS, 'cx', svg.getAttribute('width') + diff);
       circle.setAttributeNS(svgNS, 'cy', 5);
       circle.setAttributeNS(svgNS, 'r', 5);
       svg.appendChild(circle);
