a1d051bb15e2003eeebd6f9aa258408c443f2358
Joseph Swetnam <jbswetnam@gmail.com>
2015-06-20 13:05:51 -0400

    recolor data by all or row only

--- csv_to_json.js
+++ csv_to_json.js
@@ -1,11 +1,35 @@
 var fs = require('fs');
 var moment = require('moment-timezone');
 
-var filename = './fairlakes/6.1-6.12_pm';
+var contract = process.argv[2];
 
-fs.readFile(filename, 'utf-8', function(err, file) {
+var stopData = require('./' + contract + '/stop_data.js').stopData || [];
+if(process.argv.slice(-1)[0] === 'clear') stopData = [];
+
+var dataFiles = process.argv.slice(3, -1);
+if(!dataFiles[0] || dataFiles[0] === 'all') {
+  dataFiles = fs.readdirSync('./' + contract).filter(function(filename) {
+    var datePart = /^[0-9]{1,2}\.[0-9]{1,2}\-[0-9]{1,2}\.[0-9]{1,2}/.exec(filename);
+    if(datePart && !dataFiles[0]) {
+      var lastDate = stopData.slice(-1)[0].scheduledTime.format('M.D').split('.');
+      var thisDate = datePart[0].split('-')[0].split('.');
+      return thisDate[0] > lastDate[0] || (thisDate[0] === lastDate[0] && thisDate[1] > lastDate[1])
+    } else {
+      return !!datePart;
+    }
+  });
+}
+
+var contractInfo = require('./' + contract + '/contract_info.js');
+if(!contractInfo.hasOwnProperty('sortByRoute')) contractInfo.sortByRoute = true;
+contractInfo.minDiff = contractInfo.minDiff || 0;
+contractInfo.maxDiff = contractInfo.maxDiff || 0;
+contractInfo.allStops = contractInfo.allStops || [];
+contractInfo.goodData = contractInfo.goodData || 0;
+
+dataFiles.forEach(function(filename) {
+  var file = fs.readFileSync('./' + contract + '/' + filename, 'utf-8');
   var splitLines = file.split('\n');
-  var stopRecords = [];
   splitLines.slice(1).forEach(function(line) {
     if(line.trim()) {
       var splitLine = line.split(',');
@@ -15,6 +39,7 @@ fs.readFile(filename, 'utf-8', function(err, file) {
         vehicleName: splitLine[11],
         scheduledTime: moment.tz(splitLine[14], 'M/D/YYYY h:mm:ss A', 'America/New_York')
       };
+      // create arrivalTime & departureTime
       [[12, 'arrivalTime'], [13, 'departureTime']].forEach(function(subarr) {
         if(splitLine[subarr[0]]) {
           stopRecord[subarr[1]] = moment.tz(splitLine[subarr[0]], 'M/D/YYYY h:mm:ss A', 'America/New_York');
@@ -22,32 +47,68 @@ fs.readFile(filename, 'utf-8', function(err, file) {
           stopRecord[subarr[1]] = undefined;
         }
       });
+      // create arriveDiff, update maxDiff & minDiff
       if(stopRecord.arrivalTime) {
         stopRecord.arriveDiff = stopRecord.arrivalTime.diff(stopRecord.scheduledTime, 'minutes');
+        if(stopRecord.arriveDiff > contractInfo.maxDiff) contractInfo.maxDiff = stopRecord.arriveDiff;
+        if(stopRecord.arriveDiff < contractInfo.minDiff) contractInfo.minDiff = stopRecord.arriveDiff;
+        contractInfo.goodData += 1;
       } else {
         stopRecord.arriveDiff = undefined;
       }
-      stopRecords.push(stopRecord);
+      // populate allStops:
+      var stopNameAndTime = stopRecord.stopName + stopRecord.scheduledTime.format('Hmm');
+      if(contractInfo.allStops.indexOf(stopNameAndTime) === -1) {
+        contractInfo.allStops.push(stopNameAndTime);
+      }
+      stopData.push(stopRecord);
     }
   });
-  var newFile = 'var moment = require(\'moment\');\n\nexports.stopData = [\n';
-  stopRecords.forEach(function(row, rowInd) {
-    newFile += '{\n';
-    Object.keys(row).forEach(function(key, keyInd, keys) {
-      newFile += '  ' + key + ': ';
-      if(moment.isMoment(row[key])) {
-        newFile += 'moment("' + row[key].toISOString() + '")';
-      } else {
-        if(typeof row[key] === 'string') {
-          newFile += '"' + row[key] + '"';
-        } else {
-          newFile += row[key];
-        }
-      }
-      newFile += (keyInd === keys.length - 1 ? '\n' : ',\n');
-    });
-    newFile += '}' + (rowInd === stopRecords.length - 1 ? '];' : ',\n');
-  });
-  
-  fs.writeFile(filename + '.js', newFile);
 });
+
+stopData.sort(function(a, b) {
+  if(contractInfo.sortByRoute) {
+    if(a.routeName > b.routeName) return 1;
+    if(a.routeName < b.routeName) return -1;
+  }
+  if(a.scheduledTime.isAfter(b.scheduledTime)) return 1;
+  if(b.scheduledTime.isAfter(a.scheduledTime)) return -1;
+  return 0;
+});
+
+var newFile = 'var moment = require(\'moment\');\n\nexports.stopData = [\n' +
+  stopData.map(function(row) {
+      return '{' +
+        Object.keys(row).map(function(key) {
+          var str = key + ':';
+          if(moment.isMoment(row[key])) {
+            return str + 'moment("' + row[key].toISOString() + '")';
+          } else if(typeof row[key] === 'string') {
+            return str + '"' + row[key] + '"';
+          } else {
+            return str + row[key];
+          }
+        }).join(',')
+      + '}';
+    }).join(',\n') +
+'\n];'
+
+fs.writeFile('./' + contract + '/stop_data.js', newFile);
+
+var newContractInfo = Object.keys(contractInfo).map(function(key) {
+  var str = 'exports.' + key + ' = ';
+  str += (function representValue(value) {
+    if(typeof value === 'string') {
+      return '"' + value + '"';
+    } else if(Array.isArray(value)) {
+      return '[\n  ' + value.map(function(inner) {
+        return representValue(inner);
+      }).join(',\n  ') + '\n]';
+    } else {
+      return value;
+    }
+  })(contractInfo[key]);
+  return str + ';';
+}).join('\n');
+
+fs.writeFile('./' + contract + '/contract_info.js', newContractInfo);
--- generate_html.js
+++ generate_html.js
@@ -2,112 +2,78 @@ var fs = require('fs');
 var moment = require('moment-timezone');
 var cheerio = require('cheerio');
 
-var routeInfo = {
-//  contract: 'fairlakes',
-//  sortByRoute: true,
-  contract: 'mitre',
-  sortByRoute: false,
-  stopData: [],
-  allStops: [],
-  minDiff: 0,
-  maxDiff: 0,
-  earliestDate: undefined,
-  latestDate: undefined
-};
+var contract = process.argv[2];
+var contractInfo = require('./' + contract + '/contract_info.js');
+var stopData = require('./' + contract + '/stop_data.js').stopData;
 
-fs.readdirSync(routeInfo.contract).forEach(function(filename) {
-  if(/js$/.test(filename)) {
-    var loaded = require('./' + routeInfo.contract + '/' + filename).stopData;
-    routeInfo.stopData = routeInfo.stopData.concat(loaded);
-  }
-});
-
-routeInfo.stopData.sort(function(a, b) {
-  if(routeInfo.sortByRoute) {
-    if(a.routeName > b.routeName) return 1;
-    if(a.routeName < b.routeName) return -1;
-  }
-  if(a.scheduledTime.isAfter(b.scheduledTime)) return 1;
-  if(b.scheduledTime.isAfter(a.scheduledTime)) return -1;
-  return 0;
-});
-
-routeInfo.stopData.forEach(function(stopDescr) {
-  var stopNameTime = stopDescr.stopName + stopDescr.scheduledTime.format('Hmm');
-  if(!(routeInfo.allStops.indexOf(stopNameTime) > -1)) {
-    routeInfo.allStops.push(stopNameTime);
-  }
-  routeInfo.minDiff = Math.min(stopDescr.arriveDiff || 0, routeInfo.minDiff);
-  routeInfo.maxDiff = Math.max(stopDescr.arriveDiff || 0, routeInfo.maxDiff);
-
-  if(!routeInfo.earliestDate) {
-    routeInfo.earliestDate = stopDescr.scheduledTime;
-  } else if(routeInfo.earliestDate.isAfter(stopDescr.scheduledTime)) {
-    routeInfo.earliestDate = stopDescr.scheduledTime;
-  }
-
-  if(!routeInfo.latestDate) {
-    routeInfo.latestDate = stopDescr.scheduledTime;
-  } else if(stopDescr.scheduledTime.isAfter(routeInfo.latestDate)) {
-    routeInfo.latestDate = stopDescr.scheduledTime;
-  }
-});
-
-var minDiffEven = routeInfo.minDiff % 2 ? routeInfo.minDiff - 1 : routeInfo.minDiff;
-var maxDiffEven = routeInfo.maxDiff % 2 ? routeInfo.maxDiff + 1 : routeInfo.maxDiff;
+var minDiffEven = contractInfo.minDiff % 2 ? contractInfo.minDiff - 1 : contractInfo.minDiff;
+var maxDiffEven = contractInfo.maxDiff % 2 ? contractInfo.maxDiff + 1 : contractInfo.maxDiff;
 
 var $ = cheerio.load(fs.readFileSync('./index.html', 'utf-8'));
-var table = $('#data-table');
-var td = $('<td>');
-td.append($('<b>').text(routeInfo.earliestDate.format('M/D/YY') + ' - ' + routeInfo.latestDate.format('M/D/YY')));
-var tr = $('<tr>').append(td);
-for(var diff = minDiffEven; diff <= maxDiffEven; diff += 2) {
-  var sign = diff < 0 ? '-' : diff ? '+' : '';
-  var td = $('<td class="diff-td">').append(sign + '<br>' + Math.abs(diff));
-  if(diff) {
-    td.attr({ style: 'min-width:23px;max-width:23px;', colspan: '2' });
+
+if(process.argv[3] === 'nodata') {
+  var table = cheerio.load(fs.readFileSync('./' + contract + '/index.html', 'utf-8'))('#data-table');
+  $('#data-table').html(table.html());
+} else {
+  var table = $('#data-table');
+  var td = $('<td>');
+  var dateRange = stopData[0].scheduledTime.format('M/D/YY') + ' - ' + stopData.slice(-1)[0].scheduledTime.format('M/D/YY');
+  td.append($('<b>').text(dateRange));
+  var tr = $('<tr>').append(td);
+  for(var diff = minDiffEven; diff <= maxDiffEven; diff += 2) {
+    var sign = diff < 0 ? '-' : diff ? '+' : '';
+    var td = $('<td class="diff-td">').append(sign + '<br>' + Math.abs(diff));
+    if(diff) {
+      td.attr({ style: 'min-width:23px;max-width:23px;', colspan: '2' });
+    }
+    tr.append(td);
   }
-  tr.append(td);
+  table.append(tr).append(tr.clone());
+  
+  stopData.forEach(function(stopDescr, index) {
+    var display = function(time) { return time.format('h:mm A'); }
+    var camelizeName = function(stopName) { return stopName.toLowerCase().replace(/ /g,'_'); }
+  
+    var trId = camelizeName(stopDescr.stopName) + stopDescr.scheduledTime.format('HHmm');
+    var tr = $('#' + trId);
+  
+    if(!tr.length) {
+      tr = $('<tr>').attr({
+        id: trId,
+        'data-stop': camelizeName(stopDescr.stopName)
+      });
+      if(index) {
+        tr.attr('data-prevstop', camelizeName(stopData[index - 1].stopName));
+      }
+      var td = $('<td>');
+      table.append(tr.append(td.append('<b>' + display(stopDescr.scheduledTime) + '</b> ' + stopDescr.stopName)));
+   
+      for(var i = minDiffEven; i <= maxDiffEven; i++) {
+        var diffTd = $('<td>').attr({ class: 'diff-td', 'data-diff': i });
+        tr.append(diffTd);
+      }
+    }
+  
+    if(typeof stopDescr.arriveDiff !== 'undefined') {
+      var diffTd = tr.find('[data-diff=' + stopDescr.arriveDiff + ']');
+  
+      var toolTip = diffTd.find('.tooltip');
+      if(!toolTip.length) {
+        toolTip = $('<div>').addClass('tooltip').append($('<div>').text(stopDescr.arrivalTime.format('h:mma')));
+        diffTd.append(toolTip);
+      }
+      toolTip.append($('<div class="date-listing">').text(stopDescr.arrivalTime.format('M/D/YY')));
+  
+      var allAvg = toolTip.find('.date-listing').length / (stopData.length / contractInfo.allStops.length);
+      if(allAvg >= 1) allAvg = "1.0";
+      diffTd.css('background-color', 'rgba(' +
+        (stopDescr.arriveDiff > 0 ? 255 : 0) + ',' +
+        (stopDescr.arriveDiff ? 0 : 255) + ',' +
+        (stopDescr.arriveDiff < 0 ? 255 : 0) + ',' +
+        allAvg + ')'
+      ).attr('data-all-avg', allAvg);
+    }
+  });
 }
-table.append(tr).append(tr.clone());
 
-routeInfo.stopData.forEach(function(stopDescr) {
-  var display = function(time) { return time.format('h:mm A'); }
-
-  var trId = stopDescr.stopName.toLowerCase().replace(/ /g,'_') + stopDescr.scheduledTime.format('HHmm');
-  var tr = $('#' + trId);
-
-  if(!tr.length) {
-    tr = $('<tr>').attr('id', trId);
-    var td = $('<td>');
-    table.append(tr.append(td.append('<b>' + display(stopDescr.scheduledTime) + '</b> ' + stopDescr.stopName)));
- 
-    if(stopDescr.stopName === 'Vienna Metro') tr.addClass('border-top');
-
-    for(var i = minDiffEven; i <= maxDiffEven; i++) {
-      var diffTd = $('<td>').attr({ class: 'diff-td', 'data-diff': i });
-      tr.append(diffTd);
-    }
-  }
-
-  if(typeof stopDescr.arriveDiff !== 'undefined') {
-    var diffTd = tr.find('[data-diff=' + stopDescr.arriveDiff + ']');
-
-    var toolTip = diffTd.find('.tooltip');
-    if(!toolTip.length) {
-      toolTip = $('<div>').addClass('tooltip').append($('<div>').text(stopDescr.arrivalTime.format('h:mma')));
-      diffTd.append(toolTip);
-    }
-    toolTip.append($('<div>').text(stopDescr.arrivalTime.format('M/D/YY')));
-
-    var quant = (toolTip.find('div').length - 1) / (routeInfo.stopData.length / routeInfo.allStops.length);
-    diffTd.css('background-color', 'rgba(' +
-      (stopDescr.arriveDiff > 0 ? 255 : 0) + ',' +
-      (stopDescr.arriveDiff ? 0 : 255) + ',' +
-      (stopDescr.arriveDiff < 0 ? 255 : 0) + ',' +
-      quant + ')'
-    );
-  }
-});
-
-fs.writeFile(['.', routeInfo.contract, 'index.html'].join('/'), $.html());
+fs.writeFile(['.', contract, 'index.html'].join('/'), $.html());
